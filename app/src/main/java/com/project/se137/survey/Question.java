0c4cc938596a460693c0917e6b23255984f6b080
Johnny Nguyen <johnnytn.mail@gmail.com>
2015-11-05 22:50:08 -0800

    Added Survey Template
    Added layout file for survey. We will make the app so that we can
    easily extend our program to switch out between different sets of
    questions.

---
+++ app/src/main/java/com/project/se137/survey/Question.java
@@ -0,0 +1,29 @@
+package com.project.se137.survey;
+
+/**
+ * Created by Johnny on 11/5/15.
+ */
+public class Question {
+    private String mQuestion;
+    private String mAnswer;
+
+    public Question(String q) {
+
+    }
+
+    public String getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(String question) {
+        mQuestion = question;
+    }
+
+    public String getAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(String answer) {
+        mAnswer = answer;
+    }
+}
