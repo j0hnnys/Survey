c41c6e2fba0ff33c30574c9c3379b583bb4e2b07
calvin-leung <calvinl.main@gmail.com>
2015-11-23 15:54:48 -0800

    Added intents for main menu

--- app/src/main/java/com/project/se137/survey/MainActivity.java
+++ app/src/main/java/com/project/se137/survey/MainActivity.java
@@ -1,9 +1,8 @@
 package com.project.se137.survey;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-
-import com.project.se137.survey.SingleFragmentActivity;
-import com.project.se137.survey.SurveyFragment;
+import android.view.View;
 
 /**
  * Created by calvin on 11/23/2015.
@@ -13,5 +12,19 @@ public class MainActivity extends SingleFragmentActivity {
     @Override
     protected Fragment getFragment() {
         return new MainFragment();
+    }
+
+    public void onCreateSurveyClicked(View v){
+        Intent createSurveyIntent = new Intent(this, CreateSurveyActivity.class);
+        startActivity(createSurveyIntent);
+    }
+
+    public void onViewSurveyClicked(View v){
+        Intent surveyIntent = new Intent(this, SurveyActivity.class);
+        startActivity(surveyIntent);
+    }
+
+    public void onViewResultClicked(View v){
+
     }
 }
--- app/src/main/java/com/project/se137/survey/MainFragment.java
+++ app/src/main/java/com/project/se137/survey/MainFragment.java
@@ -3,13 +3,9 @@ package com.project.se137.survey;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
-import java.util.ArrayList;
 
 /**
  * Created by calvin on 11/23/2015.
